8ace357ec3b4e4dd3a0bc66c4ad1d7417de2d88b
evgenijbelov <beles123@bk.ru>
2022-12-02 16:36:48 +0300

    z25

--- z25/Program.cs
+++ z25/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//НЕ ИСПОЛЬЗОВАТЬ MATH.POW
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.Clear();
+Console.WriteLine("Напишите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Напишите второе число");
+int b = int.Parse(Console.ReadLine());
+int result =1;
+int [] arr = new int [b];
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = a;
+}
+for (int j = 0; j < arr.Length; j++)
+{
+result*=arr[j];
+}
+Console.WriteLine(result);
+

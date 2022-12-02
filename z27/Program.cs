2a109801f9dd122a04fb2a19d20d6eb58856e97a
evgenijbelov <beles123@bk.ru>
2022-12-02 19:05:24 +0300

    все кроме доп

--- z27/Program.cs
+++ z27/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.Clear();
+Console.WriteLine("Напишите число");
+int a = int.Parse(Console.ReadLine());
+int s =0;
+while (a>0)
+{
+s+=a%10;
+a/=10;
+}
+Console.WriteLine(s);
+
--- z29/Program.cs
+++ z29/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов,
+// предоставить возможность пользователю ввести числа и вывести их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+Console.Clear();
+
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите элемент массива: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    array[i] = x;
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
